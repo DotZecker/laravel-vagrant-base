1ca74a151d9fc9904b188ca006d8b40eff55599b
Sergi GP <sergigp85@gmail.com>
2013-12-21 13:50:16 +0100

    Fix problemas permisos storage

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ sudo apt-get update
 sudo add-apt-repository -y ppa:ondrej/php5
 sudo apt-get update
 sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core
+sudo sed -i 's/APACHE_RUN_USER=.*/APACHE_RUN_USER=vagrant/g' /etc/apache2/envvars
+sudo sed -i 's/APACHE_RUN_GROUP=.*/APACHE_RUN_GROUP=www-data/g' /etc/apache2/envvars
 
 # Xdebug
 sudo apt-get install -y php5-xdebug
